d33adc5942e92fe048fb61d5cc797d2b84da969b
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2024-01-27 00:54:56 +0530

    Channel creation logic

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,8 @@ import CreateServerModal from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
 import InviteModal from "@/components/modals/invite-modal";
 import EditServerModal from "@/components/modals/edit-server-modal";
+import MembersModal from "@/components/modals/members-modal";
+import CreateChannelModal from "../modals/create-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -16,6 +18,8 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
     </>
   );
 };

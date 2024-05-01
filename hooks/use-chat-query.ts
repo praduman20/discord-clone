62ae31fe2b1d5d309148072cca25daf80404e09a
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2024-05-02 00:03:42 +0530

    use-chat-query commit

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -40,7 +40,6 @@ export const useChatQuery = ({
       queryFn: fetchMessages,
       getNextPageParam: (lastPage) => lastPage?.nextCursor,
       refetchInterval: isConnected ? false : 1000,
-      initialPageParam: undefined,
     });
 
   return {

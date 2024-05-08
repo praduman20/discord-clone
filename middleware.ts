d357ae127e93de08448ebe35c1ffebcef5727694
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2024-05-08 23:23:58 +0530

    middleware changes_v3

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,13 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing", "/"],
+  publicRoutes: [
+    "/",
+    "/sign-in(.)",
+    "/sign-up(.)",
+    "/api(.)",
+    "/api/uploadthing",
+  ],
 });
 
 export const config = {
